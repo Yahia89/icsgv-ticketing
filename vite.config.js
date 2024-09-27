25e01525ba023aca1d47202d14b115031672e781
Yahia Alhejoj <yahiaalhejoj@Macmini.lan>
2024-09-27 00:08:17 -0700

    vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/icsgv-ticketing/', // Add this line to tell Vite the correct base URL
   plugins: [react()],
-})
+});
